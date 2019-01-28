ec7774b422e9dace07004846c97a163b67565003
Carlos Cesar Caballero Diaz <ccesar@daxslab.com>
2019-01-28 11:36:55 -0500

    remove unused code

--- ron/templates/gluon_template.py
+++ ron/templates/gluon_template.py
@@ -17,10 +17,4 @@ class GluonTemplate(BaseTemplate):
         dirname = os.path.dirname(self.filename)
         return self.tpl(path=dirname, filename=self.filename, context=_defaults)
 
-    def loader(self, name):
-        fname = self.search(name, self.lookup)
-        if not fname: return
-        with open(fname, "rb") as f:
-            return f.read().decode(self.encoding)
-
 gluon_template = functools.partial(template, template_adapter=GluonTemplate)

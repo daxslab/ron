32b110cbbcb46ab7687422f7951834fb66d41945
Carlos Cesar Caballero Diaz <ccesar@daxslab.com>
2019-06-23 22:30:22 -0400

    initial static files support

--- ron/app.py
+++ ron/app.py
@@ -1,3 +1,7 @@
+import os
+
+from bottle import static_file
+
 from ron.base import Module
 from ron.base.singleton import Singleton
 
@@ -13,6 +17,7 @@ class Application(Module, metaclass=Singleton):
         # self.__name__ = name
 
         Module.__init__(self, config=config, catchall=catchall, autojson=autojson)
+        self._expose_statics()
         # self.load_components()
 
     def get_with_middleware(self):
@@ -25,3 +30,12 @@ class Application(Module, metaclass=Singleton):
         for middleware_data in self.middlewares:
             app = middleware_data['class'](app, **middleware_data.get('options', {}))
         return app
+
+    def _expose_statics(self):
+        """
+        Expose statics folder
+        """
+        @self.route('/static/<filename:path>')
+        @self.route('/static/_<version:re:\d+\.\d+\.\d+>/<filename:path>')
+        def server_static(filename, path='', version=None):
+            return static_file(filename, root=os.path.join(path, 'static'))

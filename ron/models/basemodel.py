32e5f9b08acedba731c4213751c4061e97c7fbc0
Carlos Cesar Caballero Diaz <ccesar@daxslab.com>
2019-06-23 20:51:20 -0400

    include peewee-validates based validation in models

--- ron/models/basemodel.py
+++ ron/models/basemodel.py
@@ -1,4 +1,5 @@
 from peewee import Model
+from peewee_validates import ModelValidator
 
 from ron import Application
 
@@ -7,6 +8,24 @@ class BaseModel(Model):
     """
     Base class for peewee ORM models
     """
+
+    def __init__(self, *args, **kwargs):
+        Model.__init__(self, *args, **kwargs)
+
+        self._validator = ModelValidator(self)
+
+    def validate(self, data=None):
+        if data:
+            return self._validator.validate(data)
+        else:
+            return self._validator.validate()
+
+    def validator(self):
+        return self._validator
+
+    def update_model(self, data):
+        for key,value in data.items():
+            setattr(self,key,value)
 
     class Meta(object):
         database = Application().db().proxy

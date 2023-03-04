97f96e27e10dd574ed09c0197898bd6141bf3bf1
Carlos Cesar Caballero Díaz <ccesar@daxslab.com>
2023-03-04 16:42:59 -0500

    Add helpers package to setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ from distutils.core import setup
 setup(
     name='ron',
     version='0.1',
-    packages=['ron', 'ron.base', 'ron.caching', 'ron.exceptions', 'ron.templates', 'ron.web', 'ron.widgets', 'ron.models'],
+    packages=['ron', 'ron.base', 'ron.caching', 'ron.exceptions', 'ron.templates', 'ron.web', 'ron.widgets', 'ron.models', 'ron.helpers'],
     url='ron.daxslab.com',
     license='MIT',
     author='Daxslab',
